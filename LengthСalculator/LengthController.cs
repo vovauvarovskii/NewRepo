fce4d132f1b4dfd5657f3e9c110b5b900b93815b
Vova <Vova@DESKTOP-HBPECCS>
2022-06-27 21:18:14 +0300

    Добавьте файлы проекта.

---
+++ LengthСalculator/LengthController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LengthСalculator
+{
+    internal class LengthController
+    {
+        public List<string> LengthUnit { get; set; } = new List<string>()
+        {
+             "метр" , "километр" , "сантиметр" , "миллиметр"
+        };
+
+        internal object MetematicCalc(string inUint, string outUint, double valueLength)
+        {
+             if (string.Compare(inUint, outUint) == 0)
+                return valueLength;
+
+            if (inUint == "метр" && outUint == "километр") 
+                return valueLength / (double) 1000;
+
+            if (inUint == "метр" && outUint == "сантиметр") 
+                return valueLength * 100;
+
+
+            if (inUint == "метр" && outUint == "миллиметр") 
+                return valueLength  * (double)1000;
+
+
+            if (inUint == "километр" && outUint == "метр") 
+                return valueLength * (double)1000;
+
+
+
+            if (inUint == "километр" && outUint == "сантиметр") 
+                return valueLength * (double)1000 * 100;
+
+
+            if (inUint == "километр" && outUint == "миллиметр") 
+                return valueLength * (double)1000 * 100 *100;
+
+            if (inUint == "сантиметр" && outUint == "миллиметр") 
+                return valueLength *10;
+
+            return -1;
+
+        }
+    }
+
+
+}
---
+++ LengthСalculator/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LengthСalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private LengthController controller; 
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+            controller = new LengthController();
+        }
+
+        /// <summary>
+        /// загрузка формы
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            LengthController controller = new LengthController();
+            cbIn.ItemsSource = controller.LengthUnit; // контент  для комбобоксов 
+            cbOut.ItemsSource = controller.LengthUnit; // контент  для комбобоксов 
+        }
+
+        private void btGo_Click(object sender, RoutedEventArgs e)
+        {
+            if(cbIn.SelectedIndex < 0 || cbIn.SelectedIndex > cbIn.Items.Count)
+            {
+                MessageBox.Show("Неверные  параметры ");
+                return;
+            }
+
+            if (cbOut.SelectedIndex < 0 || cbOut.SelectedIndex > cbOut.Items.Count)
+            {
+                MessageBox.Show("Неверные  параметры ");
+                return;
+            }
+
+            try
+            {
+                var valueLength = Convert.ToDouble(tbvalue.Text);
+                string inUint = (string)cbIn.SelectedItem;
+                string outUint = (string)cbOut.SelectedItem;
+
+                lbRez.Content = $"Ответ: {controller.MetematicCalc(inUint, outUint, valueLength)} {outUint}ов ";
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
